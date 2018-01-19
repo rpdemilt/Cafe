be3a5a71fe296930dcb93ebf666613cb9ce3dbb1
kobemhlee <kobemhlee@gmail.com>
2018-01-18 17:18:11 -0800

    Order Class
    (not done yet)

---
+++ Order.java
@@ -0,0 +1,38 @@
+public class Order {
+
+	private double price;
+	private int time; //time variable, in minutes
+	private int orderNumber; 
+	
+	
+	public double price()
+	{
+		return price;
+	}
+
+	public void setPrice(double p)
+	{
+		price = p;
+	}
+
+	public int time()
+	{
+		return time;
+	}
+
+	public void setPrice(int t)
+	{
+		time = t;
+	}
+
+	public int orderNumber()
+	{
+		return orderNumber;
+	}
+
+	public void setOrderNumber(int o)
+	{
+		orderNumber = o;
+	}
+
+}
